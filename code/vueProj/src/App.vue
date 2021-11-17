4b3ce09d33962390381e87fe1c937edaf858ca77
dennis <55745095+twistedPan@users.noreply.github.com>
2021-11-17 10:45:33 +0100

    Update App.vue

--- code/vueProj/src/App.vue
+++ code/vueProj/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
+    <p>This is Nav</p>
+    <router-view/>
   </div>
-  <p>This is Nav</p>
-  <router-view/>
 </template>
 
 <script>
